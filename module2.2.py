b9560dce3f43cc9795f5a3af4ec0f28975f28303
Biven444 <biven2010@gmail.com>
2024-10-26 12:54:53 +0300

    Update module2.2.py

--- module2.2.py
+++ module2.2.py
@@ -1,9 +1,11 @@
 number = int(input( 'Пропиши число 1: '))
 number_1 = int(input('Пропиши число 2: '))
 number_2 = int(input('Пропиши число 3: '))
-if number_2 == number_1 or number == number_1 or number_2 == number:
-    print(2)
-elif number  == number_1 == number_2:
+if number_2 == number_1 and number == number_1 and number_2 == number:
     print(3)
+
+elif number_2 == number_1 or number == number_1 or number_2 == number:
+    print(2)
+
 else:
     print(0)
